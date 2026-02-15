c0cada28229270c71b4351d8c4461f98e8bf807e
Adrian Kisa <adrianchangwany@gmail.com>
2026-02-15 22:07:09 +0300

    fix: Remove migrations from build.sh for Render build phase compatibility

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
-python manage.py migrate --noinput
 
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
