60475c9a414c7bbf1d4293aae3bcd945dbd4451d
Adrian Kisa <adrianchangwany@gmail.com>
2026-02-26 21:51:15 +0300

    final fixed app registry issue

--- cybercraft/asgi.py
+++ cybercraft/asgi.py
@@ -1,16 +1,21 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cybercraft.settings")
+
+# Setup Django BEFORE importing routing modules
+django.setup()
+
 import services.websites.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cybercraft.settings')
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            services.websites.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(services.websites.routing.websocket_urlpatterns)
+        ),
+    }
+)
