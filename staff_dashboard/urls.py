e14bcc7fd8465d62148d2351b7b543121d11e13d
Adrian Kisa <adrianchangwany@gmail.com>
2026-03-09 09:44:17 +0300

    complete staff dashboard refactor

--- staff_dashboard/urls.py
+++ staff_dashboard/urls.py
@@ -6,4 +6,6 @@ app_name = 'staff_dashboard'
 
 urlpatterns = [
     path('dashboard/', views.StaffDashboardStatsView.as_view(), name='dashboard'),
+    path('learners/', views.StaffLearnerListView.as_view(), name='learners'),
+    path('payments/', views.StaffPaymentListView.as_view(), name='payments'),
 ]
