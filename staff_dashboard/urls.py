9272ddb63fa2cb96f1bbe8af8c53e09730f22c4c
Adrian Kisa <adrianchangwany@gmail.com>
2026-03-09 23:46:12 +0300

    changed staff dashb oard back to initial design

--- staff_dashboard/urls.py
+++ staff_dashboard/urls.py
@@ -6,6 +6,4 @@ app_name = 'staff_dashboard'
 
 urlpatterns = [
     path('dashboard/', views.StaffDashboardStatsView.as_view(), name='dashboard'),
-    path('learners/', views.StaffLearnerListView.as_view(), name='learners'),
-    path('payments/', views.StaffPaymentListView.as_view(), name='payments'),
 ]
